40af7ad47561d3ed0cb7d0ae299ad4c456f85b93
Charlie Jiang <cqjjjzr@126.com>
2015-06-08 16:53:03 +0800

    Initial Commit.

---
+++ SetTpsCommands/src/main/java/com/charlie/settpscommands/SetTpsCommands.java
@@ -0,0 +1,59 @@
+package com.charlie.settpscommands;
+
+import java.io.File;
+import java.util.Timer;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import com.charlie.earth2me.essentials.EssentialsTimer;
+
+public final class SetTpsCommands extends JavaPlugin {
+	private Timer t;
+	public static boolean enabled = true;
+	public EssentialsTimer et = new EssentialsTimer();
+	@Override
+	public boolean onCommand(CommandSender sender, Command command,
+			String label, String[] args) {
+		if(command.getName().equalsIgnoreCase("stc")){
+			if(args.length < 1 || args.length > 1){
+				sender.sendMessage(command.getUsage());
+				return false;
+			}
+			switch(args[0]){
+			case "reload":
+				this.reloadConfig();
+				t.cancel();
+				t = new Timer();
+				t.schedule(new TpsChecker(this),0,getConfig().getLong("interval"));
+				sender.sendMessage("重新加载成功");
+				return true;
+			default:
+				sender.sendMessage(command.getUsage());
+				return false;
+			}
+		}
+		return false;
+	}
+	@Override
+	public void onEnable() {
+		getLogger().info("SetTpsCommands 0.2");
+		if(!(new File(this.getDataFolder(),"config.yml").exists())){
+			getLogger().info("Config not found. Generating default config...");
+			this.saveDefaultConfig();
+			this.reloadConfig();
+		}
+		this.getServer().getScheduler().scheduleSyncRepeatingTask(this, et, 1000,50);
+		enabled = getConfig().getBoolean("enabled");
+		if(enabled)
+			getLogger().info("开始检测，时间间隔："+getConfig().getInt("interval",2000)+"ms, TPS下限："+getConfig().getInt("tpslimit")+"命令："+getConfig().getStringList("commands")+" 提示信息："+getConfig().getInt("message"));
+		t = new Timer();
+		t.schedule(new TpsChecker(this),0,getConfig().getLong("interval"));
+	}
+	@Override
+	public void onDisable() {
+		t.cancel();
+		getLogger().info("SetTpsCommands 0.2 已卸载");
+	}
+}
---
+++ SetTpsCommands/src/main/java/com/charlie/settpscommands/TpsChecker.java
@@ -0,0 +1,61 @@
+package com.charlie.settpscommands;
+
+import java.util.List;
+import java.util.TimerTask;
+import java.util.concurrent.TimeUnit;
+
+import org.bukkit.Bukkit;
+
+public class TpsChecker extends TimerTask {
+	private final SetTpsCommands plugin;
+	/*private long mills = System.currentTimeMillis();
+	private int time;*/
+	private double TPS = 20.0D;
+	private List<String> commands;
+	private String message;
+	private double tpslimit;
+	private List<Integer> dc;
+	public TpsChecker(SetTpsCommands p){
+		plugin = p;
+		commands = plugin.getConfig().getStringList("commands");
+		message = plugin.getConfig().getString("message");
+		tpslimit = plugin.getConfig().getDouble("tpslimit");
+		dc = plugin.getConfig().getIntegerList("downcount");
+	}
+	@Override
+	public void run() {
+		try{
+			this.TPS = plugin.et.getAverageTPS();
+			/*this.time += 1;
+			if (this.time == 200) {
+				long outmils = System.currentTimeMillis();
+				double r = ((outmils - this.mills) / 50.0D - 200.0D) / 10.0D;
+				double tick = 20.0D - r;
+				BigDecimal bd = new BigDecimal(tick).setScale(2, RoundingMode.HALF_EVEN);
+				tick = Double.valueOf(bd.doubleValue()).doubleValue();
+				if (tick >= 21.0D)
+					tick = 20.0D - (tick - 20.0D);
+				else if (tick >= 19.899999999999999D)
+					tick = 20.0D;
+				this.TPS = tick;
+				this.time = 0;
+				this.mills = System.currentTimeMillis();
+			}*/
+			if(this.TPS < tpslimit){
+				for(int i = 0;i < dc.size() - 1;i++){
+				/*for(int i = 0;i < dc.size();i++){*/
+					//if(dc.get(i) < 1) break;
+					plugin.getServer().broadcastMessage(String.format(message, dc.get(i)));
+					TimeUnit.SECONDS.sleep(dc.get(i) - dc.get(i + 1));
+					/*if(i == dc.size() - 1)
+						TimeUnit.SECONDS.sleep(dc.get(i));
+					else
+						TimeUnit.SECONDS.sleep(dc.get(i) - dc.get(i + 1));*/
+					for(String c : commands){
+						Bukkit.dispatchCommand(Bukkit.getConsoleSender(), c);
+					}
+				}
+			}
+		}catch(Exception e){return;}
+	}
+}
